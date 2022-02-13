6a5a05029fb59a1f9d29f58bbbe8a603e615284c
Nate Bliton <natebliton@gmail.com>
2022-02-12 21:56:38 -0500

    gets hummingbird moving poorly

--- FractalV2/Assets/Scripts/Gameplay/PlayerMover.cs
+++ FractalV2/Assets/Scripts/Gameplay/PlayerMover.cs
@@ -9,13 +9,13 @@ public class PlayerMover : MonoBehaviour
 
     public float movementSpeed = 3.0f;
 
-    Vector2 movement = new Vector2();
+    private Vector2 movement = new Vector2();
 
-    Animator animator;
+    private Animator animator;
 
     string animationState = "AnimationState";
 
-    Rigidbody2D rb2D;
+    private Rigidbody2D rb2D;
 
     // SpriteRenderer fireLeft;
     // SpriteRenderer fireRight;
@@ -27,20 +27,12 @@ public class PlayerMover : MonoBehaviour
 
     enum PlayerStates
     {
-        landedRight = 0,
-        landedLeft = 1,
-        toHoverRight=2,
-        toHoverLeft=3,
-        hoverRight=4,
+        flyRight=1,
+        hoverRight=2,
+        landedRight = 3,
+        landedLeft = 4,
         hoverLeft=5,
-        toFlyRight=6,
-        toFlyLeft=7,
-        flyRight=8,
-        flyLeft=9,
-        flyToHoverRight=10,
-        flyToHoverLeft=11,
-        landingRight=12,
-        landingLeft=13
+        flyLeft=6
     }
 
     enum VerticalState
@@ -60,6 +52,7 @@ public class PlayerMover : MonoBehaviour
 
     [SerializeField]
     PlayerStates playerState = PlayerStates.landedRight;
+    PlayerStates playerStateDisplayed = PlayerStates.landedRight;
 
     // Start is called before the first frame update
     private void Start()
@@ -87,44 +80,41 @@ public class PlayerMover : MonoBehaviour
         MoveCharacter();
     }
 
-    private void OnMouseDown()    
-    {
-    }
-
     private void MoveCharacter()
     {
-        if(Input.GetMouseButtonDown(0)) {
-            print("Pressed left click.");
-            if(playerState == PlayerStates.landedRight) {
-                playerState = PlayerStates.landedLeft;
+        // check if there is a new target location, 
+        // and if so, set velocity to move in that direction
+
+        // or, get input from keyboard
+        float xIn = Input.GetAxisRaw("Horizontal");
+        float yIn = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(xIn, yIn);
+        movement *= movementSpeed;
+        rb2D.AddForce(movement, ForceMode2D.Force);
+        
+        Vector2 vel = rb2D.velocity;
+        if(vel.magnitude > 0f) {
+            if(vel.magnitude > 3f) {
+                if(vel.x > 0f) {
+                    playerState = PlayerStates.flyRight;
+                } else {
+                    playerState = PlayerStates.flyLeft;
+                }
             } else {
-                playerState = PlayerStates.landedRight;
+                if(vel.x > 0f) {
+                    playerState = PlayerStates.hoverRight;
+                } else {
+                    playerState = PlayerStates.hoverLeft;
+                }
             }
-            animator.SetInteger(animationState,
-                (int)playerState);
-            print("clicked");
+
+        }
+        if(xIn != 0 || yIn != 0) {
+            print(xIn + " " + yIn + " " + vel.magnitude + " " + vel.x);
+            print(playerState);
         }
 
-        if(Input.GetMouseButtonDown(1)){
-            print("Pressed right click.");
-            playerState = PlayerStates.hoverRight;
-            animator.SetInteger(animationState,
-                (int)playerState);
-        }
-        
-        if(Input.GetMouseButtonDown(2)){
-            print("Pressed middle click.");
-            playerState = PlayerStates.hoverLeft;
-            animator.SetInteger(animationState,
-                (int)playerState);
-        }
 
-        movement.x = Input.GetAxisRaw("Horizontal");
-        
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        rb2D.velocity = movement * movementSpeed;
-       // print(rb2D.velocity.y + "is Y");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -134,126 +124,20 @@ public class PlayerMover : MonoBehaviour
 
     private void UpdateState()
     {
+        switch (playerState) {
+            case PlayerStates.landedRight:
+                break;
+        }
+        if (playerState != playerStateDisplayed) {
+            animator.SetInteger(animationState, (int)playerState);
+            playerStateDisplayed = playerState;
+        }
+
         // if landed
         if(verticalState == VerticalState.landed) {
 
         }
-        // if falling, or if movement.y is positive, go to hover
-        // if not, walk if movement.x is positive
-        // else, state is landed if not already set
-        // if speed is high, tip forward
 
-        // if above midway, flip upside down
-        /*
-        if(transform.position.y > 0.5 && verticalState != VerticalState.landInvert)
-        {
-            verticalState = VerticalState.floatingInvert;
-        }
-
-        if(transform.position.y < -0.5 && verticalState != VerticalState.land)
-        {
-            verticalState = VerticalState.floating;
-        }
-
-        if (verticalState == VerticalState.land)
-        {
-            if (movement.x > 0)
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.walkRight);
-            }
-            else if (movement.x < 0)
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.walkLeft);
-            }
-            else
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.idleRight);
-            }
-        }
-        else if (verticalState == VerticalState.landInvert)
-        {
-            if (movement.x > 0)
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.walkRightInvert);
-            }
-            else if (movement.x < 0)
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.walkLeftInvert);
-            }
-            else
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.idleRightInvert);
-            }
-        }
-
-        else if (verticalState == VerticalState.floating)
-        {
-            // first check we are right-side-up
-            if (animator.GetInteger(animationState) != (int)PlayerStates.flyIdle &&
-                animator.GetInteger(animationState) != (int)PlayerStates.flyLeft &&
-                animator.GetInteger(animationState) != (int)PlayerStates.flyRight)
-            {
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.flipRightInvert);
-            }
-
-            // then, pick flying or not
-            if (animator.GetInteger(animationState) != (int)PlayerStates.flipRightInvert)
-            { 
-                if (movement.x > 0)
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyRight);
-                }
-                else if (movement.x < 0)
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyLeft);
-                }
-                else
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyIdle);
-                }
-            }
-        }
-        else if (verticalState == VerticalState.floatingInvert)
-        {
-            // first check if we are right side up
-            if (animator.GetInteger(animationState) != (int)PlayerStates.flyIdleInvert &&
-                   animator.GetInteger(animationState) != (int)PlayerStates.flyLeftInvert &&
-                   animator.GetInteger(animationState) != (int)PlayerStates.flyRightInvert)
-            { 
-                animator.SetInteger(animationState,
-                    (int)PlayerStates.flipRight);
-            }
-
-            // then, pick flying or not
-            if (animator.GetInteger(animationState) != (int)PlayerStates.flipRight)
-            {
-                if (movement.x > 0)
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyRightInvert);
-                }
-                else if (movement.x < 0)
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyLeftInvert);
-                }
-                else
-                {
-                    animator.SetInteger(animationState,
-                        (int)PlayerStates.flyIdleInvert);
-                }
-            }
-        }*/
     }
 
 /*
