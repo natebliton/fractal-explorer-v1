b67b30a763d6b08a10a7e99667e7a2e7feb9de13
natebliton <natebliton@gmail.com>
2022-02-24 17:20:56 -0500

    iteration on hummingbird animation

--- FractalV2/Assets/Scripts/Gameplay/PlayerMover.cs
+++ FractalV2/Assets/Scripts/Gameplay/PlayerMover.cs
@@ -6,14 +6,31 @@ using UnityEngine.Events;
 public class PlayerMover : MonoBehaviour
 {
     // PlayerDamageEvent playerDamageEvent = new PlayerDamageEvent();
+    private Animator animator;
+    string animationState = "AnimationState";
+    private string currentState;
+    private const string LANDED_L = "landed-L";
+    private const string LANDED_R = "landed-R";
 
+    private const string TOHOVER_L = "me-h-toHover-L";
+    private const string TOHOVER_R = "me-h-toHover-R";
+    private const string HOVER_L = "me-h-hover-L";
+    private const string HOVER_R = "me-h-hover-R";
+    private const string FLY_L = "me-h-fly-L";
+    private const string FLY_R = "me-h-fly-R";
+    private const string TOFLY_L = "me-h-htofly-L";
+    private const string TOFLY_R = "me-h-htofly-R";
+    private const string FTOHOVER_L = "me-h-ftoHover-L";
+    private const string FTOHOVER_R = "me-h-ftoHover-R";
+    private const string TOLAND_L = "me-h-toland-L";
+    private const string TOLAND_R = "me-h-toland-R";
+    
+        
+    
     public float movementSpeed = 3.0f;
 
     private Vector2 movement = new Vector2();
 
-    private Animator animator;
-
-    string animationState = "AnimationState";
 
     private Rigidbody2D rb2D;
 
@@ -53,6 +70,18 @@ public class PlayerMover : MonoBehaviour
     [SerializeField]
     PlayerStates playerState = PlayerStates.landedRight;
     PlayerStates playerStateDisplayed = PlayerStates.landedRight;
+
+    #region Methods
+
+    
+    void ChangeAnimationState(string newState)
+    {
+        // guard against calling current state
+        if(currentState == newState) return;
+
+        // play the animation
+        animator.Play(newState);
+    }
 
     // Start is called before the first frame update
     private void Start()
@@ -119,15 +148,65 @@ public class PlayerMover : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-   
+        Vector3 hit = collision.contacts[0].normal;
+        print(hit);
+        float angle = Vector3.Angle(hit, Vector3.up);
+
+        if (Mathf.Approximately(angle, 0))
+        {
+            //Down
+           print("Landing");
+           if(rb2D.velocity.x > 0) {
+               playerState = PlayerStates.landedRight;
+           } else {
+               playerState = PlayerStates.landedLeft;
+           }
+           rb2D.velocity = new Vector2(0f,0f);
+        }
+        /*
+        if(Mathf.Approximately(angle, 180))
+        {
+            //Up
+            print("Up");
+        }
+        if(Mathf.Approximately(angle, 90)){
+            // Sides
+            Vector3 cross = Vector3.Cross(Vector3.forward, hit);
+            if (cross.y > 0)
+            { // left side of the player
+                print("Left");
+            }
+            else
+            { // right side of the player
+                print("Right");
+            }
+        }*/
     }
 
     private void UpdateState()
     {
         switch (playerState) {
             case PlayerStates.landedRight:
+                ChangeAnimationState(LANDED_R);
                 break;
+            case PlayerStates.landedLeft:
+                ChangeAnimationState(LANDED_L);
+                break;
+            case PlayerStates.hoverLeft:
+                ChangeAnimationState(HOVER_L);
+                break;
+            case PlayerStates.hoverRight:
+                ChangeAnimationState(HOVER_R);
+                break;
+            case PlayerStates.flyLeft:
+                ChangeAnimationState(FLY_L);
+                break;
+            case PlayerStates.flyRight:
+                ChangeAnimationState(FLY_R);
+                break;
+            
         }
+        /*
         if (playerState != playerStateDisplayed) {
             animator.SetInteger(animationState, (int)playerState);
             playerStateDisplayed = playerState;
@@ -137,7 +216,7 @@ public class PlayerMover : MonoBehaviour
         if(verticalState == VerticalState.landed) {
 
         }
-
+*/
     }
 
 /*
@@ -147,4 +226,5 @@ public class PlayerMover : MonoBehaviour
         playerDamageEvent.AddListener(listener);
     }
     */
+    #endregion
 }
