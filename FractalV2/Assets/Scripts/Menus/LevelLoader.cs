dc210fd58b1526eed3d66a2af7e541f1080eb4cf
Nate Bliton <natebliton@gmail.com>
2022-03-16 11:00:00 -0400

    PARTIAL work on menu scene fading

---
+++ FractalV2/Assets/Scripts/Menus/LevelLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+    public void LoadNextScene(string nextScene){
+        StartCoroutine(LoadScene(nextScene));
+    }
+    IEnumerator LoadScene(string nextScene) {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
